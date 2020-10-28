f06372ff00311875e4953e74fb7dbd7d0fc0f1a2
Orko Sinha <37596785+orkosinha@users.noreply.github.com>
2020-10-28 14:03:44 -0400

    Strengthen rules

--- .eslintrc.js
+++ .eslintrc.js
@@ -2,13 +2,10 @@ module.exports = {
     extends: 'airbnb-typescript-prettier',
     "rules": {
       'class-methods-use-this': 0,
-      'import/no-cycle': 0,
       'no-plusplus': 0,
       'no-restricted-syntax': 0,
-      'no-use-before-define': 0,
       'array-callback-return': 0,
       '@typescript-eslint/explicit-module-boundary-types': 0,
-      '@typescript-eslint/no-explicit-any': 0,
       '@typescript-eslint/no-unused-vars': 0
     },
     // This gets rid of weird react error for non-react project
