a1dbd542e738e6015dbf2520dfc0ec0677169564
Tedi Mitiku <tedi.m52@gmail.com>
2021-11-09 20:24:06 -0500

    Implement User entity, UserRepo, and UserResolver (#33)
    * Correct port mapping issues with docker
    
    * Add setup for notifications including user repo, entity, resolver and notification repo
    
    * Run prettier
    
    * Implement android and ios notifications
    
    * Implement function to notify users when new article posted
    
    * Add user model, repo, and resolver
    
    * Implement following and unfollowing publications
    
    * deprecate old notification code
    
    * Change notification to deviceType
    
    * Make getUsersFollowingPublication query efficient using mongoose filter
    
    * Delete extra comment
    
    * Factor out pubIndex

--- src/app.ts
+++ src/app.ts
@@ -1,24 +1,25 @@
 import 'reflect-metadata';
 import cron from 'node-cron';
+import Express from 'express';
+import { buildSchema } from 'type-graphql';
 import { ApolloServer } from 'apollo-server-express';
 import ArticleResolver from './resolvers/ArticleResolver';
 import ArticleRepo from './repos/ArticleRepo';
-import Express from 'express';
 import PublicationRepo from './repos/PublicationRepo';
 import PublicationResolver from './resolvers/PublicationResolver';
-import { buildSchema } from 'type-graphql';
+import UserResolver from './resolvers/UserResolver';
 import { dbConnection } from './db/DBConnection';
 
 const main = async () => {
   const schema = await buildSchema({
-    resolvers: [ArticleResolver, PublicationResolver],
+    resolvers: [ArticleResolver, PublicationResolver, UserResolver],
     emitSchemaFile: true,
     validate: false,
   });
 
   await dbConnection();
 
-  //Prefill publication data
+  // Prefill publication data
   await PublicationRepo.addPublicationsToDB();
 
   const server = new ApolloServer({
---
+++ src/repos/UserRepo.ts
@@ -0,0 +1,72 @@
+import { v4 as uuidv4 } from 'uuid';
+import { User, UserModel } from '../entities/User';
+import { PublicationID } from '../common/types';
+
+/**
+ * Create new pseudo suser.
+ */
+const createUser = async (
+  deviceToken: string,
+  followedPublicationsIDs: string[],
+  deviceType: string,
+): Promise<User> => {
+  // create PublicationID obejcts from string of followed publications
+  const followedPublications = followedPublicationsIDs.map((id) => {
+    return Object.assign(new PublicationID(), { id });
+  });
+
+  const uuid = uuidv4();
+  const newUser = Object.assign(new User(), {
+    uuid,
+    deviceToken,
+    followedPublications,
+    deviceType,
+  });
+
+  return UserModel.create(newUser);
+};
+
+/**
+ * Adds publication of pubID to user represented by uuid's followedPublications.
+ */
+const followPublication = async (uuid: string, pubID: string): Promise<User> => {
+  const user = await UserModel.findOne({ uuid });
+  user.followedPublications.push(Object.assign(new PublicationID(), { id: pubID }));
+  return user.save();
+};
+
+/**
+ * Deletes publication of pubID from user represented by uuid's followedPublications.
+ */
+const unfollowPublication = async (uuid: string, pubID: string): Promise<User> => {
+  const user = await UserModel.findOne({ uuid });
+  const pubIDs = user.followedPublications.map((pubIDObject) => {
+    return pubIDObject.id;
+  });
+  const pubIndex = pubIDs.indexOf(pubID);
+  if (pubIndex === -1) {
+    return user;
+  }
+  user.followedPublications.splice(pubIndex, 1);
+  return user.save();
+};
+
+const getUserByUUID = async (uuid: string): Promise<User> => {
+  return UserModel.findOne({ uuid });
+};
+
+/**
+ * Return all users who follow a publication.
+ */
+const getUsersFollowingPublication = async (pubID: PublicationID): Promise<User[]> => {
+  const users = await UserModel.find({ followedPublications: pubID });
+  return users;
+};
+
+export default {
+  createUser,
+  getUserByUUID,
+  getUsersFollowingPublication,
+  followPublication,
+  unfollowPublication,
+};
