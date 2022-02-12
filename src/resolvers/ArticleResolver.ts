d31dc7fa8faf180cd2e6e610b140f70b4fe9b182
Kidus Zegeye <kmz25@cornell.edu>
2022-02-12 14:19:52 -0500

    Add documentation to all queries

--- src/resolvers/ArticleResolver.ts
+++ src/resolvers/ArticleResolver.ts
@@ -6,35 +6,36 @@ import UserRepo from '../repos/UserRepo';
 
 @Resolver((_of) => Article)
 class ArticleResolver {
-  @Query((_returns) => Article, { nullable: true })
+  @Query((_returns) => Article, { nullable: true, description: 'Returns a single <Article> via the given <id>' })
   async getArticleByID(@Arg('id') id: string) {
     return ArticleRepo.getArticleByID(id);
   }
 
-  @Query((_returns) => [Article], { nullable: false })
+  @Query((_returns) => [Article], { nullable: false, description: 'Returns a list of <Articles> via the given list of <ids>' })
   async getArticlesByIDs(@Arg('ids', (type) => [String]) ids: string[]) {
     return ArticleRepo.getArticlesByIDs(ids);
   }
 
-  @Query((_returns) => [Article], { nullable: false })
+  @Query((_returns) => [Article], { nullable: false, description: 'Returns a list of <Articles> of size <limit>. Default <limit> is '+ DEFAULT_LIMIT })
   async getAllArticles(@Arg('limit', { defaultValue: DEFAULT_LIMIT }) limit: number) {
     const articles = await ArticleRepo.getAllArticles(limit);
     return articles;
   }
 
-  @Query((_returns) => [Article], { nullable: false })
+  @Query((_returns) => [Article], { nullable: false, description: 'Returns a list of <Articles> via the given <publicationID>'})
   async getArticlesByPublicationID(@Arg('publicationID') publicationID: string) {
     return ArticleRepo.getArticlesByPublicationID(publicationID);
   }
 
-  @Query((_returns) => [Article], { nullable: false })
+  @Query((_returns) => [Article], { nullable: false, description: 'Returns a list of <Articles> via the given list of <publicationIDs>'})
   async getArticlesByPublicationIDs(
     @Arg('publicationIDs', (type) => [String]) publicationIDs: string[],
   ) {
     return ArticleRepo.getArticlesByPublicationIDs(publicationIDs);
   }
 
-  @Query((_returns) => [Article], { nullable: false })
+  @Query((_returns) => [Article], { nullable: false, description: `Returns a list of <Articles> <since> a given date, limited by <limit>. 
+  <since> is formatted as an compliant RFC 2822 timestamp. Valid examples include: "2019-01-31", "Aug 9, 1995", "Wed, 09 Aug 1995 00:00:00", etc. Default <limit> is `+ DEFAULT_LIMIT })
   async getArticlesAfterDate(
     @Arg('since') since: string,
     @Arg('limit', { defaultValue: DEFAULT_LIMIT }) limit: number,
@@ -42,12 +43,12 @@ class ArticleResolver {
     return ArticleRepo.getArticlesAfterDate(since, limit);
   }
 
-  @Query((_returns) => [Article], { nullable: false })
+  @Query((_returns) => [Article], { nullable: false, description: 'Returns a list of trending <Articles> of size <limit>. Default <limit> is '+ DEFAULT_LIMIT })
   async getTrendingArticles(@Arg('limit', { defaultValue: DEFAULT_LIMIT }) limit: number) {
     return ArticleRepo.getTrendingArticles(limit);
   }
 
-  @FieldResolver((_returns) => Number)
+  @FieldResolver((_returns) => Number, {description: 'The trendiness score of an <Article>'} )
   async trendiness(@Root() article: Article): Promise<number> {
     const presentDate = new Date().getTime();
     // Due to the way Mongo interprets 'article' object,
@@ -55,12 +56,13 @@ class ArticleResolver {
     return article['_doc'].shoutouts / (presentDate - article['_doc'].date.getTime()); // eslint-disable-line
   }
 
-  @FieldResolver((_returns) => Boolean)
+  @FieldResolver((_returns) => Boolean, {description: 'If an <Article> contains not suitable for work content'})
   async nsfw(@Root() article: Article): Promise<boolean> {
     return ArticleRepo.checkProfanity(article['_doc'].title); //eslint-disable-line
   }
 
-  @Mutation((_returns) => Article, { nullable: true })
+  @Mutation((_returns) => Article, { nullable: true, description: `Increments the shoutouts of an <Article> with the given <id>.
+  Increments the numShoutouts given of the user with the given [uuid].`})
   async incrementShoutouts(@Arg('uuid') uuid: string, @Arg('id') id: string) {
     UserRepo.incrementShoutouts(uuid);
     return ArticleRepo.incrementShoutouts(id);
--- src/resolvers/UserResolver.ts
+++ src/resolvers/UserResolver.ts
@@ -1,3 +1,4 @@
+import { valueFromASTUntyped } from 'graphql';
 import { Resolver, Mutation, Arg, Query } from 'type-graphql';
 import { User } from '../entities/User';
 import UserRepo from '../repos/UserRepo';
@@ -5,12 +6,13 @@ import WeeklyDebriefRepo from '../repos/WeeklyDebriefRepo';
 
 @Resolver((_of) => User)
 class UserResolver {
-  @Query((_returns) => User, { nullable: true })
+  @Query((_returns) => User, { nullable: true, description: 'Returns a single [User] via a given [uuid]'})
   async getUser(@Arg('uuid') uuid: string) {
     return UserRepo.getUserByUUID(uuid);
   }
 
-  @Mutation((_returns) => User)
+  @Mutation((_returns) => User, {description: `Creates a single <User> via given <deviceToken>, <followedPulbications>,
+   and <deviceType>. Will not create a user if one with the same <deviceToken> already exists.`})
   async createUser(
     @Arg('deviceToken') deviceToken: string,
     @Arg('followedPublications', (type) => [String]) followedPublicationsIDs: string[],
@@ -20,31 +22,31 @@ class UserResolver {
     return user;
   }
 
-  @Mutation((_returns) => User, { nullable: true })
+  @Mutation((_returns) => User, { nullable: true, description: 'Lets the user from a given <uuid> follow the <Publication> given by <pubID>' })
   async followPublication(@Arg('uuid') uuid: string, @Arg('pubID') pubID: string) {
     const user = await UserRepo.followPublication(uuid, pubID);
     return user;
   }
 
-  @Mutation((_returns) => User, { nullable: true })
+  @Mutation((_returns) => User, { nullable: true, description: 'Lets the user from a given <uuid> unfollow the <Publication> given by <pubID>' })
   async unfollowPublication(@Arg('uuid') uuid: string, @Arg('pubID') pubID: string) {
     const user = await UserRepo.unfollowPublication(uuid, pubID);
     return user;
   }
 
-  @Mutation((_returns) => User, { nullable: true })
+  @Mutation((_returns) => User, { nullable: true, description: 'Adds the <Article> given by the <articleID> to the <User\'s> read articles'})
   async readArticle(@Arg('uuid') uuid: string, @Arg('articleID') articleID: string) {
     const user = await UserRepo.appendReadArticle(uuid, articleID);
     return user;
   }
 
-  @Mutation((_returns) => User, { nullable: true })
+  @Mutation((_returns) => User, { nullable: true, description: 'Increments the number of bookmarks for the <User> given by <uuid>'  })
   async bookmarkArticle(@Arg('uuid') uuid: string) {
     const user = await UserRepo.incrementBookmarks(uuid);
     return user;
   }
 
-  @Mutation((_returns) => [User])
+  @Mutation((_returns) => [User], {description: 'Creates and returns the Weekly Debrief for all users'})
   async getWeeklyDebrief() {
     const users = await WeeklyDebriefRepo.createWeeklyDebriefs();
     return users;
