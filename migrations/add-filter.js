3a14a6277fc8ab720beb9fa0da75f93dfc42cd54
Kidus Zegeye <kmz25@cornell.edu>
2022-02-24 21:38:13 -0500

    Implement PR changes

--- migrations/add-filter.js
+++ migrations/add-filter.js
@@ -1,8 +1,11 @@
 module.exports = {
   async up(db) {
-    let Filter = require("../node_modules/bad-words");
+     /**
+     * Adds the `isFiltered` field to all articles.
+     */
+    let Filter = require('../node_modules/bad-words');
     const filter = new Filter({emptyList: true});
-    filter.addWords("covid-19","coronavirus", "pandemic","masks","mask")
+    filter.addWords('covid-19','coronavirus', 'pandemic','masks','mask')
     const articles = await db.collection('articles').find({}).toArray();
     articles.map( async (article) => {
         await db.collection('articles').updateOne({_id: article._id}, {$set: {isFiltered: filter.isProfane(article.title)}});
@@ -11,9 +14,12 @@ module.exports = {
   },
 
   async down(db) {
+    /**
+     * Reverts the changes to the mongo database.
+     */
     const articles = await db.collection('articles').find({}).toArray();
     articles.map( async (article) => {
-      await db.collection('articles').updateOne({_id: article._id}, {$unset: {isFiltered: ""}});
+      await db.collection('articles').updateOne({_id: article._id}, {$unset: {isFiltered: ''}});
       return article;
     });
     },
