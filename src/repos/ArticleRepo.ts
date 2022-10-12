c2361491dacfbac93b7ee674f94db66dab296432
Kidus Zegeye <kmz25@cornell.edu>
2022-10-12 18:19:53 -0400

    Set IS_FILTER_ACTIVE to an environment variable

--- src/repos/ArticleRepo.ts
+++ src/repos/ArticleRepo.ts
@@ -4,11 +4,12 @@ import { Article, ArticleModel } from '../entities/Article';
 import {
   DEFAULT_LIMIT,
   MAX_NUM_DAYS_OF_TRENDING_ARTICLES,
-  IS_FILTER_ACTIVE,
   FILTERED_WORDS,
   DEFAULT_OFFSET,
 } from '../common/constants';
 import { PublicationModel } from '../entities/Publication';
+
+const { IS_FILTER_ACTIVE } = process.env;
 
 function isArticleFiltered(article: Article) {
   if (IS_FILTER_ACTIVE) {
