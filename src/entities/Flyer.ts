573b70d10fa5b23419fb3eeca511914823b75428
Isaac Han <isaac.han100@gmail.com>
2023-04-21 07:30:59 -0400

    Implement community board [1/7] (#81)
    * Begin implementing FlyerRepo
    
    - implemented basic FlyerRepo functions
    - updated Flyer model to support uploading in app instead of through a google form
    
    * Implement flyers for community board
    
    - alphabetized imports and fields
    - finished implementing flyers for community board
    
    * Address pr review comments
    
    - addressed pr review comments
    - removed redundant filtering in FlyerRepo
    - added checks for null return values in FlyerRepo
    - alphabetized imports and exports

--- src/entities/Flyer.ts
+++ src/entities/Flyer.ts
@@ -9,15 +9,27 @@ export class Flyer {
 
   @Field()
   @Property()
-  flyerURL: string;
+  date: Date;
 
   @Field()
-  @Property()
-  date: Date;
+  @Property({ nullable: true })
+  description: string;
 
   @Field()
   @Property()
   imageURL: string;
+
+  @Field()
+  @Property({ default: false })
+  isTrending: boolean;
+
+  @Field()
+  @Property()
+  location: string;
+
+  @Field()
+  @Property({ default: false })
+  nsfw: boolean;
 
   @Field((type) => Organization)
   @Property({ type: () => Organization })
@@ -36,20 +48,8 @@ export class Flyer {
   title: string;
 
   @Field()
-  @Property({ default: false })
-  nsfw: boolean;
-
-  @Field()
-  @Property({ default: false })
-  isTrending: boolean;
-
-  @Field()
   @Property({ default: 0 })
   trendiness: number;
-
-  @Field()
-  @Property()
-  isFiltered: boolean;
 }
 
 export const FlyerModel = getModelForClass(Flyer);
