c0383eb8b7505edb64b3b68fa0934286469ddc3d
zachseidner1 <zachary.seidner@gmail.com>
2023-09-13 21:03:50 -0400

    Fix comments

--- migrations/update-flyers-model.js
+++ migrations/update-flyers-model.js
@@ -1,7 +1,8 @@
 module.exports = {
   up(db) {
     /**
-     * Updating Flyers model to include new fields and switch to organizations
+     * Updating flyers model to switch to singular organization object and
+     * singular organization slug
      */
     return db.collection('flyers').updateMany({}, [
       {
@@ -18,8 +19,9 @@ module.exports = {
 
   down(db) {
     /**
-     * Reset organizations field to array of size 1 containing current Flyer's
-     * organizations
+     * Reset organizations field to array of size 1 containing current flyer's
+     * organization. Resets organizationSlugs to an array of size 1 containing
+     * current flyer's organization slug.
      */
     return db.collection('flyers').updateMany({}, [
       {
